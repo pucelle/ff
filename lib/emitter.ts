4e75ed48bb2728422b868f3f04ed0c66b8d00513
Freeman Chen <pucelle@163.com>
2019-03-23 09:51:43 +0800

    Rename handler to listener

--- lib/emitter.ts
+++ lib/emitter.ts
@@ -1,7 +1,7 @@
-type EventHandler = (...args: any[]) => void
+type EventItem = (...args: any[]) => void
 
 interface EventListener {
-	handler: EventHandler
+	listener: EventItem
 	scope?: object,
 	once: boolean
 }
@@ -15,14 +15,14 @@ export class Emitter<Events = any> {
 	/**
 	 * Register listener for specified event name.
 	 * @param name The event name.
-	 * @param handler The event handler.
-	 * @param scope The scope will be binded to handler.
+	 * @param listener The event listener.
+	 * @param scope The scope will be binded to listener.
 	 */
-	on<K extends keyof Events & string>(name: K, handler: Events[K] & EventHandler, scope?: object) {
+	on<K extends keyof Events & string>(name: K, listener: Events[K] & EventItem, scope?: object) {
 		let events = this._events[name] || (this._events[name] = [])
 		
 		events.push({
-			handler,
+			listener,
 			scope,
 			once: false,
 		})
@@ -31,14 +31,14 @@ export class Emitter<Events = any> {
 	/**
 	 * Register listener for specified event name for only once.
 	 * @param name The event name.
-	 * @param handler The event handler.
-	 * @param scope The scope will be binded to handler.
+	 * @param listener The event listener.
+	 * @param scope The scope will be binded to listener.
 	 */
-	once<K extends keyof Events & string>(name: K, handler: Events[K] & EventHandler, scope?: object) {
+	once<K extends keyof Events & string>(name: K, listener: Events[K] & EventItem, scope?: object) {
 		let events = this._events[name] || (this._events[name] = [])
 
 		events.push({
-			handler,
+			listener,
 			scope,
 			once: true
 		})
@@ -47,15 +47,15 @@ export class Emitter<Events = any> {
 	/**
 	 * Stop listening specified event.
 	 * @param name The event name.
-	 * @param handler The event handler, only matched listener will be removed.
-	 * @param scope The scope binded to handler. If provided, remove listener only when scope match.
+	 * @param listener The event listener, only matched listener will be removed.
+	 * @param scope The scope binded to listener. If provided, remove listener only when scope match.
 	 */
-	off<K extends keyof Events & string>(name: K, handler: Events[K] & EventHandler, scope?: object) {
+	off<K extends keyof Events & string>(name: K, listener: Events[K] & EventItem, scope?: object) {
 		let events = this._events[name]
 		if (events) {
 			for (let i = events.length - 1; i >= 0; i--) {
 				let event = events[i]
-				if (event.handler === handler && (!scope || event.scope === scope)) {
+				if (event.listener === listener && (!scope || event.scope === scope)) {
 					events.splice(i, 1)
 				}
 			}
@@ -65,20 +65,20 @@ export class Emitter<Events = any> {
 	/**
 	 * Check if registered listener for specified event.
 	 * @param name The event name.
-	 * @param handler The event handler. If provided, will also check if the handler match.
-	 * @param scope The scope binded to handler. If provided, will additionally check if the scope match.
+	 * @param listener The event listener. If provided, will also check if the listener match.
+	 * @param scope The scope binded to listener. If provided, will additionally check if the scope match.
 	 */
-	hasListener(name: string, handler?: EventHandler, scope?: object) {
+	hasListener(name: string, listener?: EventItem, scope?: object) {
 		let events = this._events[name]
 
-		if (!handler) {
+		if (!listener) {
 			return !!events && events.length > 0
 		}
-		else if (events && handler) {
+		else if (events && listener) {
 			for (let i = 0, len = events.length; i < len; i++) {
 				let event = events[i]
 
-				if (event.handler === handler && (!scope || event.scope === scope)) {
+				if (event.listener === listener && (!scope || event.scope === scope)) {
 					return true
 				}
 			}
@@ -90,25 +90,25 @@ export class Emitter<Events = any> {
 	/**
 	 * Emit specified event with followed arguments.
 	 * @param name The event name.
-	 * @param args The arguments that will be passed to event handlers.
+	 * @param args The arguments that will be passed to event listeners.
 	 */
-	emit<K extends keyof Events & string>(name: K, ...args: Parameters<Events[K] & EventHandler>) {
+	emit<K extends keyof Events & string>(name: K, ...args: Parameters<Events[K] & EventItem>) {
 		let events = this._events[name]
 		if (events) {
 			for (let i = 0; i < events.length; i++) {
 				let event = events[i]
 
-				//the handler may call off, so must remove it before handling
+				//the listener may call off, so must remove it before handling
 				if (event.once === true) {
 					events.splice(i--, 1)
 				}
 
-				event.handler.apply(event.scope, args)
+				event.listener.apply(event.scope, args)
 			}
 		}
 	}
 
-	/** Remove all event slisteners */
+	/** Remove all event listeners */
 	removeAllListeners() {
 		this._events = {}
 	}
