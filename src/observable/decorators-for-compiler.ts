375e7121350f98850645391e49e01417dcf8c2f5
Freeman Chen <pucelle@163.com>
2024-04-23 07:41:35 +0800

    Revert "Removes `@input`"
    This reverts commit 0cb4ef184cbe14ff192a0c897daa0a956a3bebfb.

--- src/observable/decorators-for-compiler.ts
+++ src/observable/decorators-for-compiler.ts
@@ -1,9 +1,25 @@
 /** 
  * `@observable` decorates a class to make its properties become observable.
  * 
+ * If a class is decorated as observable, its all public properties becomes
+ * readonly outside of class private scope, except decorates properties as `@input`.
+ * 
  * This is only a marker, it will be removed after compiled to js codes by `@pucelle/lupos-compiler`.
  */
 export function observable(_constructor: Function) {
+	throw new Error(`Please install "@pucelle/lupos-compiler" to compile your typescript codes!`)
+}
+
+
+/** 
+ * `@input` decorates a property to make it can be assigned outside of class private scope.
+ * 
+ * If a class is decorated by `@observable`, its all public properties becomes
+ * readonly outside of class private scope, except decorates properties as `@input`.
+ * 
+ * This is only a marker, it will be removed after compiled to js codes by `@pucelle/lupos-compiler`.
+ */
+export function input(_target: any, _property: string) {
 	throw new Error(`Please install "@pucelle/lupos-compiler" to compile your typescript codes!`)
 }
 
