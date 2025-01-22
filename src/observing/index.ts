8d277decf73ee3d077f17090459f44d7e1fbf734
Freeman Chen <pucelle@163.com>
2025-01-22 19:53:33 +0800

    Removes `proxyOf`, add `trackGetDeeply`

--- src/observing/index.ts
+++ src/observing/index.ts
@@ -1,6 +1,5 @@
 export * from './barrier-queue'
 export * from './decorators'
-export * from './proxy-of'
 export * from './types'
 export * from './dependency-tracker'
 export * from './update-queue'
