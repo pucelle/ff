5e3141261b704097631d34b896b4b5b8837bc7a6
Freeman Chen <pucelle@163.com>
2025-03-06 22:09:33 +0800

    Add MouseMovement class to handle drag events

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,5 +1,6 @@
 export * from './event-firer'
 export * from './simulated-events'
+export * from './mouse-movement'
 export * as DOMEvents from './dom-events'
 export * as DOMModifiableEvents from './dom-modifiable-events'
 export * as MouseLeaveControl from './mouse-leave-control'
---
+++ src/events/mouse-movement.ts
@@ -0,0 +1,73 @@
+import * as DOMEvents from './dom-events'
+import {Point, Vector} from '../math'
+
+
+export interface MouseMovementOptions {
+
+	/** At least moves for this to start calls `onMove`. */
+	minimumMoves: number
+}
+
+const DefaultMouseMovementOptions: MouseMovementOptions = {
+	minimumMoves: 0
+}
+
+
+/** 
+ * Handle dragging like.
+ * Start by mousedown event.
+ */
+export class MouseMovement {
+
+	/** 
+	 * Specifies this to handle movement.
+	 * First parameter is movement from event start.
+	 * Second parameter is movement from previous.
+	 */
+	onMove: ((moves: Vector, latestMoves: Vector) => void) | null = null
+
+	/** Handle after movement end. */
+	onEnd: (() => void) | null = null
+
+	protected options: MouseMovementOptions
+	protected startPoint: Point
+	protected latestPosition: Point
+	protected started: boolean = false
+	
+	constructor(e: MouseEvent, options: Partial<MouseMovementOptions> = {}) {
+		this.options = {...DefaultMouseMovementOptions, ...options}
+		this.startPoint = this.latestPosition = new Point(e.clientX, e.clientY)
+
+		DOMEvents.on(document, 'mousemove', this.onDragMove, this)
+		DOMEvents.once(document, 'mouseup', this.onDragEnd, this)
+	}
+
+	protected onDragMove(e: MouseEvent) {
+		let eventPoint = new Point(e.clientX, e.clientY)
+		let moves = eventPoint.diff(this.startPoint)
+		let latestMoves = eventPoint.diff(this.latestPosition)
+
+		this.latestPosition = eventPoint
+
+		if (!this.started) {
+			if (moves.getLength() > this.options.minimumMoves) {
+				this.started = true
+			}
+			else {
+				return
+			}
+		}
+
+		this.onMove?.(moves, latestMoves)
+	}
+
+	protected onDragEnd(_e: MouseEvent) {
+		this.end()
+		this.onEnd?.()
+	}
+
+	end() {
+		DOMEvents.off(document, 'mousemove', this.onDragMove, this)
+		DOMEvents.off(document, 'mouseup', this.onDragEnd, this)
+	}
+}
