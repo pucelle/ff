19cf5a7f534529cbcfe2c4ab4b8ad1c06003973e
Freeman Chen <pucelle@163.com>
2025-11-03 15:17:16 +0800

    IntersectionWatcher becomes a class

--- src/watchers/index.ts
+++ src/watchers/index.ts
@@ -1,4 +1,4 @@
 export * as ResizeWatcher from './resize-watcher'
-export * as IntersectionWatcher from './intersection-watcher'
+export * from './intersection-watcher'
 export * as DocumentWatcher from './document-watcher'
 export * as RectWatcher from './rect-watcher'
--- src/watchers/intersection-watcher.ts
+++ src/watchers/intersection-watcher.ts
@@ -5,63 +5,63 @@ import {bindCallback} from '../utils'
 type IntersectionObserverCallback = (entry: IntersectionObserverEntry) => void
 
 
-/** 
- * Help to dispatch intersection observer callback for several elements,
- * according to a single observer.
- */
-let observer: IntersectionObserver | null = null
+export class IntersectionWatcher {
 
-/** Cache element -> bound callbacks. */
-const CallbackMap: ListMap<Element, IntersectionObserverCallback> = /*#__PURE__*/new ListMap()
+	/** 
+	 * Help to dispatch intersection observer callback for several elements,
+	 * according to a single observer.
+	 */
+	private observer: IntersectionObserver
 
+	/** Cache element -> bound callbacks. */
+	private callbackMap: ListMap<Element, IntersectionObserverCallback> = new ListMap()
 
-/** Accept intersection entries. */
-function onIntersectionCallback(entries: IntersectionObserverEntry[]) {
-	for (let entry of entries) {
-		let callbacks = CallbackMap.get(entry.target)
-		if (callbacks) {
-			for (let callback of [...callbacks]) {
-				callback(entry)
+	constructor(threshold: number[] = [0, 1]) {
+		this.observer = new IntersectionObserver(this.onIntersectionCallback.bind(this), {threshold})
+	}
+
+	/** Accept intersection entries. */
+	private onIntersectionCallback(entries: IntersectionObserverEntry[]) {
+		for (let entry of entries) {
+			let callbacks = this.callbackMap.get(entry.target)
+			if (callbacks) {
+				for (let callback of [...callbacks]) {
+					callback(entry)
+				}
 			}
 		}
 	}
-}
 
-
-/** 
- * Watch intersection of an element.
- * Get notified after element's becomes fully visible or fully invisible.
- */
-export function watch(el: Element, callback: IntersectionObserverCallback, scope: any = null) {
-	if (!observer) {
-		observer = new IntersectionObserver(onIntersectionCallback)
-	}
-
-	let boundCallback = bindCallback(callback, scope)
-
-	if (CallbackMap.has(el, boundCallback)) {
-		return
-	}
-
-	observer.observe(el)
-	CallbackMap.add(el, boundCallback)
-}
-
-
-/** 
- * Unwatch intersection of an element.
- * If `callback` omitted, unwatch all callbacks for element.
- */
-export function unwatch(el: Element, callback?: IntersectionObserverCallback, scope?: any) {
-	if (callback) {
+	/** 
+	 * Watch intersection of an element.
+	 * Get notified after element's becomes fully visible or fully invisible.
+	 */
+	watch(el: Element, callback: IntersectionObserverCallback, scope: any = null) {
 		let boundCallback = bindCallback(callback, scope)
-		CallbackMap.delete(el, boundCallback)
-	}
-	else {
-		CallbackMap.deleteOf(el)
+
+		if (this.callbackMap.has(el, boundCallback)) {
+			return
+		}
+
+		this.observer.observe(el)
+		this.callbackMap.add(el, boundCallback)
 	}
 
-	if (!CallbackMap.hasKey(el)) {
-		observer?.unobserve(el)
+	/** 
+	 * Unwatch intersection of an element.
+	 * If `callback` omitted, unwatch all callbacks for element.
+	 */
+	unwatch(el: Element, callback?: IntersectionObserverCallback, scope?: any) {
+		if (callback) {
+			let boundCallback = bindCallback(callback, scope)
+			this.callbackMap.delete(el, boundCallback)
+		}
+		else {
+			this.callbackMap.deleteOf(el)
+		}
+
+		if (!this.callbackMap.hasKey(el)) {
+			this.observer.unobserve(el)
+		}
 	}
 }
