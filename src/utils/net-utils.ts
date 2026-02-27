36ae1b6ce08dd92c71c6df34fdf5e1c790b2ecf3
Freeman Chen <pucelle@163.com>
2026-02-27 16:59:09 +0800

    Add `uploadFileWithProgress`

---
+++ src/utils/net-utils.ts
@@ -0,0 +1,42 @@
+
+import {useQuery} from './url-utils'
+
+
+/** Upload file with progress. */
+export function uploadFileWithProgress(
+	url: string,
+	query: Record<string, string | number> | null,
+	file: File,
+	onProgress?: (loaded: number, total: number) => void
+) {
+	if (query) {
+		url = useQuery(url, query)
+	}
+
+	return new Promise((resolve, reject) => {
+		let xhr = new XMLHttpRequest()
+		xhr.open('POST', url)
+
+		xhr.upload.onprogress = e => {
+			if (e.lengthComputable) {
+				console.log(e.loaded, e.total)
+				onProgress?.(e.loaded, e.total)
+			}
+		}
+
+		xhr.onload = () => {
+			if (typeof xhr.response === 'string') {
+				resolve(JSON.parse(xhr.response))
+			}
+			else if (typeof xhr.response === 'object') {
+				resolve(xhr.response)
+			}
+			else {
+				resolve(null)
+			}
+		}
+
+		xhr.onerror = reject
+		xhr.send(file)
+	})
+}
